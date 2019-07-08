a734a4efc20204c6ab8d2f5ec656c45509494bc6
aherher <37237988+aherher@users.noreply.github.com>
2019-07-08 17:21:43 +0900

    Update layout.js

--- js/layout.js
+++ js/layout.js
@@ -1,5 +1,7 @@
 $(document).ready(function(e) {
-var left_list = '<li><a href="#!">일반형</a></li><li><a href="#!">웹모바일</a></li>'
-$(left_list).prependTo.("ul.author__urls");
+  alert();
+var left_list = '<li><a href="#!">일반형</a></li><li><a href="#!">웹모바일</a></li>';
+var left_menu = $("body").find(".ul.author__urls");
+$(left_list).prependTo.(left_menu);
   
 });
